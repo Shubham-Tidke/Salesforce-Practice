3f799caf8e87f669638a0e6261df7a82fe4c00d5
Shubham-Tidke <shubham.tidke.165@gmail.com>
2022-07-07 18:31:43 +0530

    Data Binding,SLDS in LWC

--- LWC/LWC-Fundamentals/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ LWC/LWC-Fundamentals/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    name = "User";
+
+    changeHandler(event){
+        this.name = event.target.value;
+    }
+}
