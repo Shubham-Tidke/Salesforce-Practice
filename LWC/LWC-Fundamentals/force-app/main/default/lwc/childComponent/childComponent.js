4a780c50bb27c848d47020f05370812ae935a4ac
Shubham-Tidke <shubham.tidke.165@gmail.com>
2022-07-17 21:30:56 +0530

    Parent-Child Component Communication [Primitive/Non Primitive datatypes]

--- LWC/LWC-Fundamentals/force-app/main/default/lwc/childComponent/childComponent.js
+++ LWC/LWC-Fundamentals/force-app/main/default/lwc/childComponent/childComponent.js
@@ -1,3 +1,7 @@
-import { LightningElement } from 'lwc';
+import { api,LightningElement } from 'lwc';
 
-export default class ChildComponent extends LightningElement {}
+export default class ChildComponent extends LightningElement {
+    @api message //making property public with api decorator
+    @api cardHeading //camelCase property gets converted to kebab case when used as an attribute
+    @api carouselDetails //non-primitive datatype
+}
