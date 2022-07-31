760e50b456859100a8b43da5fc3d67d55e7f1816
Shubham-Tidke <shubham.tidke.165@gmail.com>
2022-07-31 16:44:25 +0530

    fetching object type,record Id and Show Toast msg component

--- LWC/LWC-Fundamentals/force-app/main/default/lwc/userInfo/userInfo.js
+++ LWC/LWC-Fundamentals/force-app/main/default/lwc/userInfo/userInfo.js
@@ -1,6 +1,11 @@
 import { LightningElement } from 'lwc';
 import hasViewAllData from '@salesforce/userPermission/ViewAllData'
+import ID from '@salesforce/user/Id' //to fetch user's ID
+import isGuest from '@salesforce/user/isGuest' // to check if user is guest or not
+
 export default class UserInfo extends LightningElement {
+    userId = ID;
+    guest = isGuest;
     get hasViewAllDataAvailable(){
         return hasViewAllData;
     }
