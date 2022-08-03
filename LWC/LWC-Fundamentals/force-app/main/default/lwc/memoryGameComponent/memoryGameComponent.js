9b2ac46f2fd3d2011b0dbdf6e20ebd008623b7c3
Shubham-Tidke <shubham.tidke.165@gmail.com>
2022-08-03 18:20:27 +0530

    Memory Game Component - logic

--- LWC/LWC-Fundamentals/force-app/main/default/lwc/memoryGameComponent/memoryGameComponent.js
+++ LWC/LWC-Fundamentals/force-app/main/default/lwc/memoryGameComponent/memoryGameComponent.js
@@ -4,6 +4,9 @@ import FONT_AWESOME from '@salesforce/resourceUrl/fontawesome'
 export default class MemoryGameComponent extends LightningElement {
 
     isLibLoaded = false;
+    openedCards=[]//to let user open only 2 cards at a time
+    moves = 0 //counting no. of moves
+    matchedCard=[]//to store matched cards
     cards = [
         {id:1, listClass:"card", type:'podcast', icon:'fa fa-podcast'},
         {id:2, listClass:"card", type:'bomb', icon:'fa fa-bomb'},
@@ -40,5 +43,49 @@ export default class MemoryGameComponent extends LightningElement {
     displayCard(event){
         let currCard = event.target;
         currCard.classList.add("open","show","disabled")//adding css classes
+        this.openedCards = this.openedCards.concat(event.target)//adding opened cards in array
+        if (this.openedCards.length == 2) {
+            this.moves = this.moves+1 //updating moves
+            if (this.openedCards[0].type === this.openedCards[1].type) {
+                this.matchedCard= this.matchedCard.concat(this.openedCards[0],this.openedCards[1])
+                this.matched()
+            }
+            else
+            this.unmatched()
+        }
+    }
+    matched(){
+        this.openedCards[0].classList.add("match","disabled")
+        this.openedCards[1].classList.add("match","disabled")
+        this.openedCards[0].classList.remove("show","open")
+        this.openedCards[1].classList.remove("show","open")
+        
+        this.matchedCard=[]
+    }
+    unmatched(){
+        this.openedCards[0].classList.add("unmatch")
+        this.openedCards[1].classList.add("unmatch")
+        this.action('DISABLE')
+        setTimeout(()=>{
+            this.openedCards[0].classList.remove("unmatch","show","open")
+            this.openedCards[1].classList.remove("unmatch","show","open")
+            this.action('ENABLE');
+            this.matchedCard=[]
+        },1100)
+    }
+    action(action){
+        let cards  = this.template.querySelectorAll('.card')
+        //converting node like array to array
+        Array.from(cards).forEach(item=>{
+            if(action === 'ENABLE'){
+               let isMatch = item.classList.contains('match')
+               if(!isMatch){
+                item.classList.remove('disabled')
+               }
+            }
+            if(action === 'DISABLE'){
+                item.classList.add('disabled')
+             }
+        })
     }
 }
