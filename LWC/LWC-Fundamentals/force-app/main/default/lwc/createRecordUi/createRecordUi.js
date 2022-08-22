74cf1db0b8d801377b89ade6fa8e0f335ef9e014
Shubham-Tidke <shubham.tidke.165@gmail.com>
2022-08-22 19:49:56 +0530

    LWC - createRecordUi adapter corrections

--- LWC/LWC-Fundamentals/force-app/main/default/lwc/createRecordUi/createRecordUi.js
+++ LWC/LWC-Fundamentals/force-app/main/default/lwc/createRecordUi/createRecordUi.js
@@ -1,7 +1,8 @@
 import { LightningElement } from 'lwc';
 import { createRecord } from 'lightning/uiRecordApi';
 import CONTACT_OBJECT from '@salesforce/schema/Contact'
-import {showToastEvent} from 'lightning/platformShowToastEvent'
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
 export default class CreateRecordUi extends LightningElement {
     formFields={}
     changeHandler(event){
@@ -9,17 +10,19 @@ export default class CreateRecordUi extends LightningElement {
         this.formFields[name] = value
     }
     createContact(){
-        const recordInput = {apiName:CONTACT_OBJECT,fields:this.formFields}
+        const recordInput = {apiName:CONTACT_OBJECT.objectApiName,fields:this.formFields}
         createRecord(recordInput).then(result=>{
-            this.showToast('Success','Contact Created');
+            this.showToast('Success!!',`${result.id}`);
+           console.log(result.id);
             this.template.querySelector('form.createForm').reset()
             this.formFields={}
         }).catch(error=>{
+            console.error(error);
             this.showToast('Error',error.body.message,'error');
         })
     }
     showToast(title,message,variant){
-        this.dispatchEvent(new showToastEvent({
+        this.dispatchEvent(new ShowToastEvent({
             title,
             message,
             variant: variant||'success'
