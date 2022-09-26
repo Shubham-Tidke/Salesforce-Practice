b20c9d739fb2872cc7481eec210372bbbfdb4ce8
Shubham-Tidke <shubham.tidke.165@gmail.com>
2022-09-26 11:59:02 +0530

    CarHub App : Car Tile component

--- LWC/LWC-Applications/CarHubApp/force-app/main/default/lwc/carTileList/carTileList.js
+++ LWC/LWC-Applications/CarHubApp/force-app/main/default/lwc/carTileList/carTileList.js
@@ -1,3 +1,17 @@
-import { LightningElement } from 'lwc';
-
-export default class CarTileList extends LightningElement {}
+import { LightningElement, wire } from 'lwc';
+import getCars from '@salesforce/apex/CarController.getCars'
+export default class CarTileList extends LightningElement {
+    cars
+    error
+    @wire(getCars)
+    carsHandler({data,error}){
+        if(data){
+            //console.log(data);
+            this.cars = data
+        }
+        if(error){
+            this.error = error
+           // console.log(error);
+        }
+    }
+}
