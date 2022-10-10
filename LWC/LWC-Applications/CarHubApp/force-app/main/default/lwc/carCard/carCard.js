8474196a85e5e67a28ac02bfe42d4055cbfc5329
Shubham-Tidke <shubham.tidke.165@gmail.com>
2022-10-10 18:00:12 +0530

    CARHUB APP : Car card design

--- LWC/LWC-Applications/CarHubApp/force-app/main/default/lwc/carCard/carCard.js
+++ LWC/LWC-Applications/CarHubApp/force-app/main/default/lwc/carCard/carCard.js
@@ -1,3 +1,22 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
+import NAMEFIELD from '@salesforce/schema/Car__c.Name'
+import PICTURE_URL_FIELD from '@salesforce/schema/Car__c.Picture_URL__c'
+import CATEGORY_FIELD from '@salesforce/schema/Car__c.Category__c'
+import MAKE_FIELD from '@salesforce/schema/Car__c.Make__c'
+import MSRP_FIELD from '@salesforce/schema/Car__c.MSRP__c'
+import FUEL_FIELD from '@salesforce/schema/Car__c.Fuel__c'
+import SEATS_FIELD from '@salesforce/schema/Car__c.Number_of_Seats__c'
+import CONTROL_FIELD from '@salesforce/schema/Car__c.Control__c'
 
-export default class CarCard extends LightningElement {}
+
+export default class CarCard extends LightningElement {
+    
+    categoryField = CATEGORY_FIELD
+    makeField = MAKE_FIELD
+    msrpField = MSRP_FIELD
+    fuelField = FUEL_FIELD
+    seatsField = SEATS_FIELD
+    controlField = CONTROL_FIELD
+    recordId ="a005i000009IRclAAG"
+    
+}
