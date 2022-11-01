27a3053d07bbabe951824f35c20cef87b34cc720
Shubham-Tidke <shubham.tidke.165@gmail.com>
2022-11-01 17:47:59 +0530

    Mass Automation Handler : Triggers display

--- LWC/LWC-Applications/Mass Automation Handler/force-app/main/default/lwc/automationHandler/automationHandler.js
+++ LWC/LWC-Applications/Mass Automation Handler/force-app/main/default/lwc/automationHandler/automationHandler.js
@@ -1,10 +1,11 @@
 import { LightningElement, wire } from 'lwc';
 import fetchAllTriggers from '@salesforce/apex/automationHandler.fetchAllTriggers';
+import fetchMdtRecords from '@salesforce/apex/automationHandler.fetchMdtRecords';
 export default class AutomationHandler extends LightningElement {
 
     triggerData;
     items = []
-    
+    mdt_records = [];
     @wire(fetchAllTriggers)
     wiredData({ error, data }) {
       if (data) {
@@ -12,14 +13,22 @@ export default class AutomationHandler extends LightningElement {
         this.triggerData = data;
         for (let index in this.triggerData) {
            this.items = [...this.items, { Object: index, TriggerName: this.triggerData[index] }];
-        }
-        console.log(this.keyList);
+        }     
+      } else if (error) {
+        console.error('Error:', error);
+      }
+    }   
+    @wire(fetchMdtRecords)
+    wiredMetaData({ error, data }) {
+      if (data) {
+        console.log('Data', data);
       } else if (error) {
         console.error('Error:', error);
       }
     }
-    toggleHandler(){
-        return true;
-    }
     
+    toggleHandler(event){
+      let tgrName = this.template.querySelector('div');
+      console.log(tgrName);
+    }
 }
