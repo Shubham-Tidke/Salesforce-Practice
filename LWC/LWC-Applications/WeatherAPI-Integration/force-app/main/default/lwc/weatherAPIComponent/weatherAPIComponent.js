45bac4b867017ee33c59381f1c4da9aa5a16f330
Shubham-Tidke <shubham.tidke.165@gmail.com>
2022-11-05 11:42:45 +0530

    Integration : WeatherAPI JS update

--- LWC/LWC-Applications/WeatherAPI-Integration/force-app/main/default/lwc/weatherAPIComponent/weatherAPIComponent.js
+++ LWC/LWC-Applications/WeatherAPI-Integration/force-app/main/default/lwc/weatherAPIComponent/weatherAPIComponent.js
@@ -1,8 +1,9 @@
 import { LightningElement } from 'lwc';
+import weatherStackAPIKey from '@salesforce/label/c.weatherStackAPIKey';
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '6995b0d2bdmsh9726f3610d1fa31p1678dcjsn0e500ff8d5c9',
+		'X-RapidAPI-Key': weatherStackAPIKey,
 		'X-RapidAPI-Host': 'aerisweather1.p.rapidapi.com'
 	}
 };
@@ -11,6 +12,13 @@ export default class WeatherAPIComponent extends LightningElement {
     location = '';
     weather
     timer
+    avgTemp
+    humidity
+    maxTemp
+    minTemp
+    primaryWeather
+    windDir
+    windSpeed
     connectedCallback(){
         this.fetchWeather();
     }
@@ -18,15 +26,23 @@ export default class WeatherAPIComponent extends LightningElement {
         fetch('https://aerisweather1.p.rapidapi.com/forecasts/'+this.location+',', options)
 	.then(response => response.json())
 	.then(data => {
-        console.log(data.response[0].periods[0]);
-        this.weather = data;
+        this.weather = data.response[0].periods[0];
+       // console.log(this.weather);
+        console.log("Weather Forecast at "+this.location);
+        console.log("Average Temperature is :"+this.weather.avgTempC+"°C");
+        console.log("Maximum Temperature Expected : "+this.weather.maxTempC+"°C");
+        console.log("Minimum Temperature Expected : "+this.weather.minTempC+"°C");
+        console.log("Humidity : "+this.weather.humidity+" g.m-3 ");
+        console.log("Wind Speed & Direction :"+this.weather.windSpeedKPH+" KMPH,"+this.weather.windDir);
+        console.log("Overall Weather will be "+this.weather.weather);
+        
     })
 	.catch(err => console.error(err));
     }
 
     getLocation(event){
         this.location = event.target.value;
-        console.log(this.location);
+       // console.log(this.location);
         window.clearTimeout(this.timer)
         this.timer = setTimeout(() => {
             this.fetchWeather()
