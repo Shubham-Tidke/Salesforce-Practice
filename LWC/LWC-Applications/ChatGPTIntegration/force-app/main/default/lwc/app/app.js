b6be3380ffab54ff39cb617c5bac4a1941be31a5
Shubham-Tidke <shubham.tidke.165@gmail.com>
2023-02-07 17:29:39 +0530

    Chat GPT SF integration

---
+++ LWC/LWC-Applications/ChatGPTIntegration/force-app/main/default/lwc/app/app.js
@@ -0,0 +1,36 @@
+import { LightningElement, track } from 'lwc';
+import chatgptKey from  '@salesforce/label/c.chatgptKey';
+
+
+export default class App extends LightningElement {
+    @track textInput
+    @track finalValue
+    @track showSpinner = false
+    handleInput(event){
+        this.textInput = event.target.value;
+    }
+    handleClick(event){
+        this.showSpinner = true
+       fetch('https://api.openai.com/v1/completions',
+       { method: 'POST',
+            headers: {
+           'Content-Type': 'application/json',
+           'Authorization': 'Bearer '+ chatgptKey 
+       },
+         body: JSON.stringify({
+            "prompt":this.textInput ,
+            "model": "text-davinci-003",
+            "temperature": 0.0,
+            "max_tokens": 4000,
+            "stream": false,
+            "top_p": 0.5
+          })
+        })
+        .then(response=>response.json())
+        .then(result=> {
+            this.showSpinner = false
+            this.finalValue = result.choices[0].text
+            console.log("response ::  "+this.finalValue)
+        })
+    }
+}
