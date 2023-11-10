14c87d891ff01baa7bd60ce58888c9788f8b674a
Shubham-Tidke <shubham.tidke.165@gmail.com>
2023-11-10 20:22:07 +0530

    News App using API : UI update

--- LWC/LWC-InterviewPrep/News-App/lwc/newsComponent/newsComponent.js
+++ LWC/LWC-InterviewPrep/News-App/lwc/newsComponent/newsComponent.js
@@ -1,18 +1,31 @@
 import { LightningElement } from 'lwc';
 import getNews from '@salesforce/apex/NewsComponentController.getNews';
-export default class NewsComponent extends LightningElement {
-
+import NewsImg from '@salesforce/resourceUrl/NewsImg';
+import { NavigationMixin } from 'lightning/navigation';
+export default class NewsComponent extends NavigationMixin(LightningElement) {
+    newsData;
+    img = NewsImg;
     connectedCallback() {
         this.fetchNews();
     }
     fetchNews(){
         getNews().then((result)=>{
             if(result){
+                this.newsData = (result.articles);
                 console.log('OUTPUT : ',result);
             }
         })
         .catch((error)=>{
             console.log('OUTPUT ERROR: ',error);
+        })
+    }
+    newsClickHandler(event){
+        console.log('OUTPUT : ',event.currentTarget.dataset.id);
+        this[NavigationMixin.Navigate]({
+            type:'standard__webPage',
+            attributes:{
+                url:event.currentTarget.dataset.id
+            }
         })
     }
 }
